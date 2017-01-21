d07f53703602635dbaada0b0e03696a8befe3804
BlueZell <xavibravo.guillen@gmail.com>
2017-01-21 14:55:05 +0100

    Stadium sound + script

---
+++ GameJam2017/Assets/Scripts/AudioController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        AudioSource Stadium_Ambience = GetComponent<AudioSource>();
+        Stadium_Ambience.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
