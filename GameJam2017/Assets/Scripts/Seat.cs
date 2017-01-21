673d496cb9b7543034faf04dc8d84ecad2ba1a08
XBlay <blay.xavi@gmail.com>
2017-01-21 09:32:01 +0100

    URSSManager & Seats

---
+++ GameJam2017/Assets/Scripts/Seat.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seat : MonoBehaviour {
+
+    public GameObject takenBy; //objeto x el que esta ocupado: si ocupedBy == null -> Sin ocupar
+    public bool canBeTookedByPlayer = false;
+    
+}
