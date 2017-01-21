d930154c99a691fac087392ce9acbcfcea7d512c
XBlay <blay.xavi@gmail.com>
2017-01-21 09:41:25 +0100

    URSSManager: Sprites Lists

--- GameJam2017/Assets/Scripts/URSSManager.cs
+++ GameJam2017/Assets/Scripts/URSSManager.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class URSSManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int players = 4;
+    public Seat[] seats;
+    public List<Seat> playerSeats;
+
+    public List<Sprite> gorro;
+    public List<Sprite> cara;
+    public List<Sprite> gafas;
+    public List<Sprite> camiseta;
+    public List<Sprite> raya;
+
+    public void FillSeats()
+    {
+        seats = null;
+        seats = FindObjectsOfType<Seat>();
+        playerSeats.Clear();
+        for (int i = 0; i < seats.Length; i++)
+        {
+            if (seats[i].canBeTookedByPlayer)
+                playerSeats.Add(seats[i]);
+        }
+    }
+
+    public void RandomizePlayers()
+    {
+        int camiseta = Random.Range(0, 4);
+    }
+        
+
 }
